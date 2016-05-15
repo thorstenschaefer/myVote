3ae493e20d6493604f7bb8d1a4ab6d75978b95b5
Thorsten Schäfer <thorstenschaefer@outlook.com>
2016-05-15 18:17:47 -0300

    Added visual representation of result

--- src/app/+view-poll/view-poll.component.ts
+++ src/app/+view-poll/view-poll.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouteSegment, OnActivate, ROUTER_DIRECTIVES } from '@angular/router';
 
-import {Poll} from '../data/poll';
+import {Poll, PollOption} from '../data/poll';
 import {PollService} from '../data/poll.service';
 import { PollQuestionComponent } from '../poll-question/poll-question.component';
 import { LoadingIndicatorComponent } from '../shared';
@@ -16,7 +16,9 @@ import { LoadingIndicatorComponent } from '../shared';
 export class ViewPollComponent implements OnInit, OnActivate {
 
   private poll: Poll;
-
+  private totalVotes: number;
+  private highestVotes: number;
+  
   constructor(
     private pollService : PollService) {
   }
@@ -27,13 +29,25 @@ export class ViewPollComponent implements OnInit, OnActivate {
   routerOnActivate(curr: RouteSegment): void {
     let pollId = curr.getParam('pollId');
     this.pollService.getById(pollId)
-      .subscribe(p => this.poll = p );
+      .subscribe(p => {
+        this.poll = p;
+        this.highestVotes = p.options.map(o => o.value).reduce((a,b) => Math.max(a,b));
+        this.totalVotes = p.options.map(o => o.value).reduce((a,b) => a+b);
+      } );
   }
 
-  private getTotalVotes() {
-    // console.log(this.poll);
-    if (this.poll === null) // debug
-      return 0;
-    return this.poll.options.map(option => option.value).reduce((a, b) => a+b);
+  // private getTotalVotes() {
+  //   // console.log(this.poll);
+  //   if (this.poll === null) // debug
+  //     return 0;
+  //   return this.poll.options.map(option => option.value).reduce((a, b) => a+b);
+  // }
+  
+  private getPercentage(option: PollOption):number {
+    return option.value / this.totalVotes;
+  }
+  
+  private isBestOption(option:PollOption):boolean {
+    return option.value === this.highestVotes;
   }
 }
